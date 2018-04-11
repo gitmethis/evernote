3ba67af2b6155ec1360e4e4eb975f35779dca944
github username <github email address>
2018-04-11 15:48:30 -0400

    fixed errors

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let user_root_url = "localhost:3000/api/v1/users"
-  let note_root_url = "localhost:3000/api/v1/notes"
-
+  let user_root_url = "http://localhost:3000/api/v1/users"
+  let note_root_url = "http://localhost:3000/api/v1/notes"
+  let configuration_post = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    }
+  }
+  let userName = document.createElement("h1")
 
   function getUser(){
-    fetch(user_root_url).then(res=> res.json())
+    fetch(user_root_url)
+      .then(res=> res.json())
+      .then(json => {
+        userName.innerText = json[0].name;
+        document.body.prepend(userName)
+      })
   }
 
   function getNotes(){
-    fetch(note_root_url).then(res => res.json())
+    fetch(note_root_url)
+      .then(res => res.json())
+      .then(json =>{
+
+      })
   }
 
-
-  function createNote(){
-    fetch("localhost:3000/api/v1/notes/new").then(res => res.json()).then(res => console.log)
+  function createNote(newPostbody){
+    configuration_post.body = JSON.stringify(newPostbody);
+    fetch(note_root_url, configuration_post).then(res => res.json())
   }
 })
